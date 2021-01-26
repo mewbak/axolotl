62bebaef921b15aca9d7d5603a912ae26e9eb32f
Aaron <aaron@nanu-c.org>
2021-01-26 18:50:43 +0100

    more fixes

--- app/handler/handler.go
+++ app/handler/handler.go
@@ -259,7 +259,7 @@ func ReceiptMessageHandler(msg *textsecure.Message) {
 
 // SyncSentHandler handle sync messages from signal desktop
 func SyncSentHandler(msg *textsecure.Message, timestamp uint64) {
-	log.Debugln("[axolotl] handle sync message", msg.Timestamp())
+	log.Debugln("[axolotl] handle sync message", msg.Timestamp(), msg.SourceUUID())
 	// use the same routine to save sync messages as incoming messages
 	buildAndSaveMessage(msg, true)
 }
